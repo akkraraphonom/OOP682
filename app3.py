7160ceb781e7102bfe93f105c5deb50a1de314c7
akkraraphon <pisit.ph.65@ubu.ac.th>
2026-01-30 16:52:57 +0700

    add all

--- app3.py
+++ app3.py
@@ -1,11 +1,11 @@
-from model.classroom import Classroom
+from model.classroom import ClassRoom
 from model.sutdent import Student
 
-oop = Classroom("OOP")  
-oop.add_student(Student(1, "Alice", 20, "P123", "S001"))
-oop.add_student(Student(2, "Bob", 22, "P124", "S002"))
+oop = ClassRoom("OOP")  
+oop.add_student(Student( "Alice", 20, "P123", "S001"))
+oop.add_student(Student( "Bob", 22, "P124", "S002"))
 print(f"Classroom {oop.name} has {len(oop)} students.")
-oop.add_student(Student(3, "Charlie", 21, "P125", "S003"))
+oop.add_student(Student( "Charlie", 21, "P125", "S003"))
 print(len(oop))
 
 for i in range(len(oop)):
